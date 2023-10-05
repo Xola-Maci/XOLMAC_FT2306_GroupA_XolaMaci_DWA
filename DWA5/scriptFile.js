64b1504cd1f7854e72bd65a3c69d1812ec948eea
Xola-Maci <137217340+Xola-Maci@users.noreply.github.com>
2023-10-05 16:28:19 +0200

    updated DWA5 but it has errors

--- DWA5/scriptFile.js
+++ DWA5/scriptFile.js
@@ -1,9 +1,44 @@
+
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
-  const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+  const { dividend, divider } = Number(Object.fromEntries(entries));
+  
+  //try {
+    const resultsDiv = dividend / divider;
+
+  if(dividend === '' || divider === ''){
+    alert('Division not performed. Both values are required in inputs. Try again')
+    throw new Error ('Invalid input');
+   }
+
+ if(!Number.isInteger(resultsDiv)){
+  result.innerText = Math.floor(resultsDiv);
+  return;
+ }
+
+ if(divider <= 0 || divider < 0){
+  alert('Division not performed. Invalid number provided. Try again');
+  throw new Error('Number is less or equal to zero');
+ }
+
+ if(isNaN(dividend) || isNaN(divider)){
+  alert('Something critical went wrong. Please reload the page!')
+  throw new Error('Invalid input: input entereed is not a numerical character')
+ }
+
+ result.innerText = resultsDiv;
+  /**} catch (error) {
+    result.innerText = error.message;
+
+    if(error.message === 'Division not performed. Invalid number provided. Try again'){
+      console.error(error, error.stack);
+    }else if(error.message === 'Something critical went wrong. Please reload the page!'){
+      document.body.innerHTML = `<h1>${error.message}</h1>`
+    }
+  }*/
+  
 });
