97caf3a870deb8f19245be0ad824664c3072b4b4
Xola-Maci <137217340+Xola-Maci@users.noreply.github.com>
2023-10-18 10:37:44 +0200

    completed DWA10

--- DWA10/script.js
+++ DWA10/script.js
@@ -1,13 +1,31 @@
+/**
+ * displays maximum count number
+ * displays minimum counting number
+ * Step amount is the number the tally counts in and in this case its one
+ * @type {number} MAX_NUMBER
+ */
 const MAX_NUMBER = 10
-const MIN_NUMBER = -5
-const STEP_AMOUNT = 5
+const MIN_NUMBER = 0
+const STEP_AMOUNT = 1
+
+/**
+ * @type {HTMLInputElement} - element where the count input goes in
+ * @type {HTMLButtonElement} - button for subtracting
+ * @type {HTMLButtonElement} - button for adding the counter
+ * @type {HTMLButtonElement} - button for resetting
+ * @type {HTMLElement} - the popup element
+ */
 
 const number = document.querySelector('[data-key="number"]')
 const subtract = document.querySelector('[data-key="subtract"]')
 const add = document.querySelector('[data-key="add"]')
+const reset = document.querySelector('[data-key="reset"]')
+const popup = document.getElementById('popup');
 
-//subtractHandler newValue
-//addHandler newValue
+/**
+ * Handler for handling the subtraction button
+ * @typedef {object} subtractHandler
+ */
 
 const subtractHandler = () => {
     const newValue = parseInt(number.value) - 1
@@ -22,6 +40,10 @@ const subtractHandler = () => {
 }
 }
 
+/**
+ * Handler for handling the addition button
+ * @typedef {object} addHandler
+ */
 const addHandler = () => {
 const newValue = parseInt(number.value) + 1
 number.value = newValue
@@ -36,6 +58,37 @@ if(newValue >= MAX_NUMBER){
 
 }
 
-subtract.addEventListener('click', subtractHandler)
+/**
+ * Handler for handling the reset button
+ * @typedef {object} resetHandler
+ */
+const resetHandler = () => {
+  number.value = '0';
+  subtract.disabled = false;
+  add.disabled = false;
+  displayPopup();
+};
 
+/**
+ * Handler for displaying the popup
+ * @typedef {object} showPopup
+ */
+const showPopup = () => {
+  popup.style.display = 'block';
+
+  setTimeout(() => {
+      hidePopup();
+  }, 5000);
+};
+
+/**
+ * Handler for closing the popup
+ * @typedef {object} closePopup
+ */
+const closePopup = () => {
+  popup.style.display = 'none';
+};
+
+subtract.addEventListener('click', subtractHandler)
 add.addEventListener('click', addHandler)
+reset.addEventListener('click',resetHandler)
